b0286a3b73a68d5e488fbdd91ae6a4328fdde491
Anthony Sauboua <saubouaanthony@gmail.com>
2022-06-23 22:56:46 +0200

    modif route idArticle

--- views/pages/article/list.php
+++ views/pages/article/list.php
@@ -1,10 +1,13 @@
 <h4>Actualité</h4>
 <?php
 foreach ($data['articles'] as $article) { ?>
-    <a href="index.php?page=show_article&id=<?= $article->getId() ?>">
+    <a href="index.php?page=show_article&idArticle=<?= $article->getId() ?>">
         <li>
             <?= $article->getTitle() ?>
         </li>
     </a>
 <?php } ?>
-<button><a href="index.php?page=add_article">Ajouter un article</a></button>
+
+<button>
+    <a href="index.php?page=add_article">Ajouter un article</a>
+</button>
