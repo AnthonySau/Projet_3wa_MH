d18f0dc7b4f4847feeb87953a62c5a9599875592
Anthony Sauboua <saubouaanthony@gmail.com>
2022-06-23 23:00:13 +0200

    delete br

--- views/pages/app/home.php
+++ views/pages/app/home.php
@@ -5,9 +5,9 @@
             <div class="bgc-mirror">
                 <h2>Bienvenue sur mon blog Monster Hunter World</h2>
                 <p>
-                    Monster Hunter est une série de jeux vidéo développée et éditée par Capcom
-                    <br>
-                    <br>
+                    Monster Hunter est une série de jeux vidéo développée et éditée par Capcom.
+                </p>
+                <p>
                     Le joueur incarne un chasseur dans un environnement fantasy qui complète des quêtes ayant principalement pour but de chasser ou capturer des monstres. <br>
                     Au fil du jeu il améliore son équipement avec les materiaux des monstres tués.
                 </p>
