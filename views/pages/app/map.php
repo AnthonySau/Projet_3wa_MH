fb9c4c2b7f7376afed170c3c0ce8462bb23566ed
Anthony Sauboua <saubouaanthony@gmail.com>
2022-07-28 17:22:59 +0200

    final css map

--- views/pages/app/map.php
+++ views/pages/app/map.php
@@ -10,6 +10,7 @@
                         <img src="./public/images/Map_Astera.jpeg" alt="village Monster hunter.">
                     </div>
                     <div class="card_content">
+                        <h5>Astera</h5>
                         <p class="card_text">Astera est la ville principale de Monster Hunter World. C'est ici que vous pourrez vous reposer entre deux chasses, prendre des quêtes, acheter du consommable et customiser votre personnages.</p>
                     </div>
                 </div>
@@ -21,6 +22,7 @@
                         <img src="./public/images/Map_foretAncien.jpeg" alt="Carte avec une foret ancienne.">
                     </div>
                     <div class="card_content">
+                        <h5>Foret Ancienne</h5>
                         <p class="card_text">La Forêt ancienne est remplie d'arbres et de végétations, facile à traverser par des moyens normaux. Elle possède une faune et une flore très diversifiée. C'est une des régions du Nouveau monde de Monster Hunter: World.</p>
                     </div>
                 </div>
@@ -32,6 +34,7 @@
                         <img src="./public/images/Map_Corail.jpeg" alt="Carte avec des montagnes et des cristeaux.">
                     </div>
                     <div class="card_content">
+                        <h5>Plateau de corail</h5>
                         <p class="card_text">Le Plateau de corail est une région étrange typique du " Nouveau monde " où pousse des sortes d’arbres de corail. De plus, de forts courants d'air animent les environs.</p>
                     </div>
                 </div>
@@ -43,6 +46,7 @@
                         <img src="./public/images/Map_desert.jpeg" alt="Carte avec du sable, le desert.">
                     </div>
                     <div class="card_content">
+                        <h5>Desert des termites</h5>
                         <p class="card_text">Le Désert des termites est une région relativement désertique divisée en deux parties : une zone totalement désertique et une autre composée de marécages qui sont directement connectés aux cours d'eau de la Forêt ancienne.</p>
                     </div>
                 </div>
@@ -54,6 +58,7 @@
                         <img src="./public/images/Map_Putride.jpeg" alt="Carte avec du miasme, la mort y habite.">
                     </div>
                     <div class="card_content">
+                        <h5>Val Putride</h5>
                         <p class="card_text">Le Val putride possède une ambiance radicalement différente des zones précédentes. Alors que le Plateau de corail est aérien et enchanteur, le Val putride est sombre, sinistre et en décomposition. Surnommée le cimetière.</p>
                     </div>
                 </div>
@@ -65,6 +70,7 @@
                         <img src="./public/images/Map_Ancien.jpeg" alt="Carte avec de la lave et des cristeaux blanc.">
                     </div>
                     <div class="card_content">
+                        <h5>Terre des Anciens</h5>
                         <p class="card_text">La Terre des anciens est très inhospitalière, l'importante activité volcanique et la chaleur extrême, rendent ces lieux très dangereux. C'est la destination principale des dragons anciens, pendant leurs migration vers le nouveaux monde.</p>
                     </div>
                 </div>
